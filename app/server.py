5cf135142441027e802cd21284d4552f5acadb81
Igor816 <morgunov.vlad2002@mail.ru>
2026-03-20 02:05:30 +0300

    creat work server

--- app/server.py
+++ app/server.py
@@ -5,5 +5,20 @@ import argparse
 from flask import Flask
 
 
-def main():
-    ...
+class Server:
+    
+    def __init__(self, port, host):
+        self.port = port
+        self.host = host
+        
+        self.app = Flask(__name__)
+
+        self.app.add_url_rule('')
+
+    def shutdown(self):
+        pass
+
+    def server_shutdown(self):
+        pass
+
+
